db3ed59f921db6e049d7db9d7b71643d25a98a09
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2022-09-25 21:23:14 -0400

    Adding places view with mock date

---
+++ views/places/index.jsx
@@ -0,0 +1,35 @@
+const React = require('react')
+
+function Def(html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+}
+
+module.exports = Def
+
+function index (data) {
+  let placesFormatted = data.places.map((place) => {
+    return (
+      <div>
+        <h2>{place.name}</h2>
+        <img src={place.pic} alt={place.name}/>
+      </div>
+    )
+  })
+  return (
+    <Def>
+        <main>
+            <h1>PLACES INDEX PAGE</h1>
+            {placesFormatted}
+        </main>
+    </Def>
+)
+}
