6ab00f0632a22a5a2feb1ada62bb03dd0a41b202
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2022-10-09 21:56:21 -0400

    adding comments

--- controllers/places.js
+++ controllers/places.js
@@ -1,3 +1,106 @@
+
+const router = require('express').Router()
+const db = require('../models')
+//get
+router.get('/', (req, res) => {
+  console.log('here')
+  db.Place.find()
+    .then((places) => {
+      console.log('places')
+      console.log(places)
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log('err')
+      console.log(err)
+      res.render('error404')
+    })
+})
+
+//post
+
+//new
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+//show
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .populate('comments')
+  .then(place => {
+      console.log(place.comments)
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+//delete
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+//edit
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
+//create BEFORE COMMENTS
+// router.post('/', (req, res) => {
+//   db.Place.create(req.body)
+//     .then(() => {
+//       res.redirect('/places')
+//     })
+//     .catch(err => {
+//       if (err && err.name == 'ValidationError') {
+//         let message = 'Validation Error: '
+//         for (var field in err.errors) {
+//             message += `${field} was ${err.errors[field].value}. `
+//             message += `${err.errors[field].message}`
+//         }
+//         console.log('Validation error message', message)
+//         res.render('places/new', { message })
+//     }
+//     else {
+//         res.render('error404')
+//     }
+//     })
+// })
+
+
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+  res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
+
 /*const router = require('express').Router()
 const places = require('../models/places.js')
 
@@ -94,7 +197,7 @@ router.get('/:id/edit', (req, res) => {
       res.render('error404')
   }
   else {
-    res.render('places/edit', { place: places[id] })
+    res.render('places/edit', { place: places[id],id })
   }
 })
 
@@ -103,68 +206,3 @@ router.get('/:id/edit', (req, res) => {
 
 module.exports = router
 */
-const router = require('express').Router()
-const nodemon = require('nodemon')
-const db = require('../models')
-
-router.get('/', (req, res) => {
-    db.Place.find()
-    .then((places) => {
-      res.render('places/index', { places })
-    })
-    .catch(err => {
-      console.log(err) 
-      res.render('error404')
-    })
-})
-
-
-router.post('/', (req, res) => {
-  res.send('POST /places stub')
-})
-
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
-
-router.get('/:id', (req, res) => {
-  db.Place.findById(req.params.id)
-  .then(place => {
-      res.render('places/show', { place })
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
-
-
-router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
-})
-
-router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
-})
-
-router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
-})
-
-router.post('/', (req, res) => {
-  db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
-
-
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
-})
-
-module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,31 +1,69 @@
 const React = require('react')
 const Def = require('../default')
 
-function show(data) {
-  return (
-    <Def>
-      <main>
-        <h1>{data.place.name}</h1>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-
-        <div>
-          <h2>Rating</h2>
-          <h3>Not Rated</h3>
+function show (data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
         </div>
-
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
-
-
-
-      </main>
-    </Def>
+      )
+    })
+  }
+  return (
+      <Def>
+        <main>
+          <div className="row">
+            ...
+          </div>
+          <hr />
+          <h2>Comments</h2>
+          {comments}
+        </main>
+      </Def>
   )
 }
 
 module.exports = show
+
+
+//BEFORE COMMENT
+// function show(data) {
+//   return (
+//     <Def>
+//       <main>
+//         <h1>{data.place.name}</h1>
+//         <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+//           Edit
+//         </a>
+
+//         <div>
+//           <h2>Rating</h2>
+//           <h3>Not Rated</h3>
+//         </div>
+
+//         <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+//           <button type="submit" className="btn btn-danger">
+//             Delete
+//           </button>
+//         </form>
+
+
+
+//       </main>
+//     </Def>
+//   )
+// }
+
+// module.exports = show
