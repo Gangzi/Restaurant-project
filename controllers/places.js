cf7598b57329496c841bf6a7d50296006ff582a4
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2022-10-11 21:44:50 -0400

    Populating data for star rating

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,7 @@
 
 const router = require('express').Router()
 const db = require('../models')
+
 //get
 router.get('/', (req, res) => {
   console.log('here')
@@ -18,59 +19,81 @@ router.get('/', (req, res) => {
 })
 
 //post
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+    .then(place => {
+      db.Comment.create(req.body)
+        .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+            .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+            })
+        })
+        .catch(err => {
+          res.render('error404')
+        })
+    })
+})
 
 //new
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
+
 //show
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-  .populate('comments')
-  .then(place => {
+    .populate('comments')
+    .then(place => {
       console.log(place.comments)
       res.render('places/show', { place })
-  })
-  .catch(err => {
+    })
+    .catch(err => {
       console.log('err', err)
       res.render('error404')
-  })
+    })
 })
 
-
-
+//update
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect(`/places/${req.params.id}`)
+    })
+    .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+    })
 })
+
 //delete
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
-})
-//edit
-router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
-})
-router.post('/:id/comment', (req, res) => {
-  console.log(req.body)
-  db.Place.findById(req.params.id)
-  .then(place => {
-      db.Comment.create(req.body)
-      .then(comment => {
-          place.comments.push(comment.id)
-          place.save()
-          .then(() => {
-              res.redirect(`/places/${req.params.id}`)
-          })
-      })
-      .catch(err => {
-          res.render('error404')
-      })
-  })
-  .catch(err => {
+  db.Place.findByIdAndDelete(req.params.id)
+    .then(place => {
+      res.redirect('/places')
+    })
+    .catch(err => {
+      console.log('err', err)
       res.render('error404')
-  })
+    })
 })
 
+//edit
+router.get('/:id/edit', (req, res) => {
+  db.Place.findById(req.params.id)
+    .then(place => {
+      res.render('places/edit', { place })
+    })
+    .catch(err => {
+      res.render('error404')
+    })
+})
+
+
+
+module.exports = router
 //create BEFORE COMMENTS
 // router.post('/', (req, res) => {
 //   db.Place.create(req.body)
@@ -95,11 +118,11 @@ router.post('/:id/comment', (req, res) => {
 
 
 
-router.delete('/:id/rant/:rantId', (req, res) => {
-  res.send('GET /places/:id/rant/:rantId stub')
-})
+// router.delete('/:id/rant/:rantId', (req, res) => {
+//   res.send('GET /places/:id/rant/:rantId stub')
+// })
 
-module.exports = router
+
 
 /*const router = require('express').Router()
 const places = require('../models/places.js')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,37 +1,44 @@
 const React = require('react')
 const Def = require('../default')
 
-function show (data) {
+function show(data) {
   let comments = (
     <h3 className="inactive">
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
-    comments = data.place.comments.map(c => {
-      return (
-        <div className="border">
-          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
-          <h4>{c.content}</h4>
-          <h3>
-            <stong>- {c.author}</stong>
-          </h3>
-          <h4>Rating: {c.stars}</h4>
-        </div>
-      )
-    })
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += 'â­ï¸'
+    }
+  
+    rating = (
+      <h3>
+        {stars} stars
+      </h3>
+    )
   }
   return (
-      <Def>
-        <main>
-          <div className="row">
-            ...
-          </div>
-          <hr />
-          <h2>Comments</h2>
-          {comments}
-        </main>
-      </Def>
+    <Def>
+      <main>
+        <div className="row">
+          ...
+        </div>
+        <hr />
+        <h2>Comments</h2>
+        {comments}
+      </main>
+    </Def>
   )
 }
 
